799f7537bcfa7381661af9f36965d00741209778
mightymazhar <msyed9784@gmail.com>
2017-03-07 16:22:04 +0600

    Update 1) a.cpp

--- 1) a.cpp
+++ 1) a.cpp
@@ -1,16 +1,16 @@
-#include <iostream>
+#include <iostream>   // Creates a library to store data.
 
-using namespace std;
+using namespace std;   
 
-int main()
+int main()                                      // Main function.
 
-{
-    string name;
+{                                                        // Starts the main function.
+    string name;                                         // Assingning an input to the main function.
 
-    cout << "Hello what is your name? " << endl;
-    cin >> name;
+    cout << "Hello what is your name? " << endl;          // Text output asking for user's input. // endl creates line gap.
+    cin >> name;                                          // Takes user's input.
 
-    cout << "Hi there " << name << "." << endl;
+    cout << "Hi there " << name << "." << endl;           // Uses the input of the user to create an output.  // endl creates line gap.
 
-    return 0;
-}
+    return 0;                                            // Terminates the program.
+}                                                        // Ends the main function.
