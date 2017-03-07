472fae321f5ca95f4eb27e10c0b3d80a0918d984
mightymazhar <msyed9784@gmail.com>
2017-03-07 15:31:22 +0600

    Add files via upload

---
+++ 1) a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    string name;
+
+    cout << "Hello what is your name? " << endl;
+    cin >> name;
+
+    cout << "Hi there " << name << "." << endl;
+
+    return 0;
+}
