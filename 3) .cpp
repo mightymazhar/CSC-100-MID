13b3fd16ad135cb15530e22570b7776fc26f1ec6
mightymazhar <msyed9784@gmail.com>
2017-03-08 14:32:14 +0600

    Add files via upload

---
+++ 3) .cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+
+{
+    string symbol;
+
+
+    cout << "Enter character with which you want a symbol: " << endl;
+    cin >> symbol;
+
+    cout << symbol << symbol << symbol << symbol << symbol << symbol << endl;
+
+    cout << symbol << endl;
+
+    cout << symbol << endl;
+
+    cout << symbol << symbol << symbol << symbol << endl;
+
+    cout << symbol << endl;
+
+    cout << symbol << endl;
+
+    cout << symbol << endl;
+
+
+
+
+
+   return 0;
+}
