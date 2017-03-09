616a8f827015d67332478f6e8240c9d2acb48df9
mightymazhar <msyed9784@gmail.com>
2017-03-09 18:53:18 +0600

    Update 15) .cpp

--- 15) .cpp
+++ 15) .cpp
@@ -1,15 +1,15 @@
-#include <iostream>
-#include <string>
-#include <cmath>
+#include <iostream> // creates librasry to store data
+#include <string> // creates library to store strings
+#include <cmath> // creates library to store floats
 
 using namespace std;
 
 int main ()
 
 {
-   string name;
-   int roll;
-   float csc;
+   string name; // includes word values for name
+   int roll;  // includes integer values for roll
+   float csc; // includes subject mark value to decimal accuracy
    float mat;
    float hea;
 
@@ -28,12 +28,12 @@ int main ()
    cout << endl << "Please enter HEA 101 marks: ";
    cin >> hea;
 
-   float total = csc + mat + hea;
-   float percentage = (total / 300)*100;
+   float total = csc + mat + hea; // adds all marks of each subject
+   float percentage = (total / 300)*100; // calculates percentage with total
 
    cout << endl << "Name: " << name << endl << "Roll: " << roll << endl << "Marks in CSC 100: " << csc << endl << "Marks in MAT 101: " << mat << endl << "Marks in HEA 101: " << hea << endl << "Total percentage for 3 courses: " << percentage << endl;
 
-   if (percentage > 80 )
+   if (percentage > 80 ) // sets condition for output with limit
    {
        cout << endl << "Congratulations! you have achieved first division" << endl;
    }
