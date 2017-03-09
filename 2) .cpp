25ad2fd987261ed3a009599458d6809f90f10d8d
mightymazhar <msyed9784@gmail.com>
2017-03-09 19:10:42 +0600

    Update 2) .cpp

--- 2) .cpp
+++ 2) .cpp
@@ -1,11 +1,12 @@
-#include <iostream>
+#include <iostream> //creates a library to store data
+#include <string> //creates library to store strings
 
 using namespace std;
 
-int main ()
+int main () // main function
 
 {
-    string name;
+    string name; // enters word values for inputs
     string date;
     string number;
 
@@ -18,7 +19,7 @@ int main ()
     cout << "What is your phone number? " << endl;
     cin >> number;
 
-    cout << endl << "So you are " << name << "." << endl << "You were born in " << date << "." << endl << "Your phone number is " << number << "." << endl;
+    cout << endl << "So you are " << name << "." << endl << "You were born in " << date << "." << endl << "Your phone number is " << number << "." << endl; // combined output including all strings
 
     return 0;
 
