0f503c8aec3c8467b704dbfcd2140854105c419b
mightymazhar <msyed9784@gmail.com>
2017-03-09 19:49:12 +0600

    Add files via upload

---
+++ 21) - 25) .cpp
@@ -0,0 +1,24 @@
+21// The fact that computer programming develops a higher sense of logic is what made me choose CS as a subject.
+
+22// My passion is to be able to adapt to any profession and be adept in multiple fields of work/employment so that I can adapt to the changing job market.
+
+23// CS develops my typing ability, my logical thinking ability, calculative ability and all of these are very helpful both in an academic career and professional career.
+
+24// a) You declare these values by typing them in and assign values to them by using a specific letter or number desired by the user.
+  // b) With these values we can make calculators, user identity storage or access systems, student grading system, temperature display or weather systems and accounting calculation systems.
+
+25// a)char 	        1 byte
+  //   int 		        2 or 4 bytes
+  //   float 		    4 byte
+  //   double 		    8 byte
+  //   string           1 or 4 bytes
+
+  // b) char : stores alphabetical characters
+  //    int  : stores integers
+  //    float: stores integers with decimal place accuracy
+  //   string: stores random data input by user
+  //   double: stores values with two times more accuracy than float
+
+  // c) integer : accurate to ones place value
+  //    float   : accurate to 6-7 decimal place
+  //    double  ; accurate to twice the decimal place as float
