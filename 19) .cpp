e8ef43795bd737e1e4006dd3e4da0363c3d66b2c
mightymazhar <msyed9784@gmail.com>
2017-03-09 19:08:36 +0600

    Update 19) .cpp

--- 19) .cpp
+++ 19) .cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-#include <string>
-#include <cmath>
+#include <iostream> //creates a library to store data
+#include <string>  //creates library to store strings
+#include <cmath>  // creates library to store floats
 
 using namespace std;
 
 int main ()
 
 {
-    float cost;
-    float sell;
+    float cost; // includes integer value for cost to decimal accuracy
+    float sell; // includes integer value for selling price to decimal accuracy
 
 
     cout << "Enter your cost price: ";
@@ -17,23 +17,23 @@ int main ()
     cout << endl << "Enter your selling price: ";
     cin >> sell;
 
-    float profit;
+    float profit; // includes output values to decimal accuracy
     float loss;
 
-    profit = sell - cost;
+    profit = sell - cost; // formula for calculating output
     loss = cost - sell;
 
-    if ( sell > cost )
+    if ( sell > cost ) // condition for choosing one of the two formulas as per user's desired output  
     {
         cout << endl << "You have made a profit of: " << profit << endl;
     }
-
-    else if ( cost > sell )
+ 
+    else if ( cost > sell ) // condition for choosing one of the two formulas as per user's desired output  
     {
         cout << endl << "You have suffered a loss of: " << loss << endl;
     }
 
-    else ;
+    else ; // condition regardless of previous conditions
     {
         cout << endl << "You have neither profit nor loss" << endl;
     }
