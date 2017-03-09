d2d5d2bcd12ecb965ca348d906533c89f7c07720
mightymazhar <msyed9784@gmail.com>
2017-03-09 19:01:44 +0600

    Update 17) .cpp

--- 17) .cpp
+++ 17) .cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <string>
-#include <cmath>
+#include <iostream> //creates a library to store data
+#include <string> //creates library to store strings
+#include <cmath> // creates library to store floats
 
 using namespace std;
 
 int main ()
 
 {
-    float temp;
+    float temp; // includes temperature values to decimal accuracy
 
     cout << "What is the temperature today?: ";
     cin >> temp;
 
-    if (temp < 0)
+    if (temp < 0) // sets certain condition for temperature output
     {
         cout << endl << "Freezing weather " << endl;
     }
 
-    else if (temp > 0 && temp < 10)
+    else if (temp > 0 && temp < 10) // sets certain condition for temperature output
     {
         cout << endl << "Very cold weather " << endl;
     }
 
-    else if (temp > 10 && temp < 20)
+    else if (temp > 10 && temp < 20) // sets certain condition for temperature output
     {
         cout << endl << "Cold weather " << endl;
     }
 
-    else if (temp > 20 && temp < 30)
+    else if (temp > 20 && temp < 30) // sets certain condition for temperature output
     {
         cout << endl << "Normal weather " << endl;
     }
 
-    else if (temp > 30 && temp < 40)
+    else if (temp > 30 && temp < 40) // sets certain condition for temperature output
     {
         cout << endl << "Hot weather " << endl;
     }
 
     else
     {
-        cout << endl << "Very hot weather" << endl;
+        cout << endl << "Very hot weather" << endl; // output rgardless of condition
     }
 
     return 0;
